0f28534c498201dc365dd3545f8a8446373f2041
ljh <654994961@qq.com>
2021-03-20 16:25:43 +0800

    Stack without using list

---
+++ Stack.py
@@ -0,0 +1,44 @@
+# Something about stack
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.head = None
+
+    def __str__(self):
+        return self.value
+
+
+class Stack:
+    def __init__(self):
+        self.node = None
+        self.size = 0
+
+    def add(self, node):
+        if not self.node:
+            self.node = node
+        else:
+            node.head = self.node
+            self.node = node
+        self.size += 1
+
+    def pop(self):
+        if not self.node:
+            return None
+        else:
+            if self.size > 0:
+                node = self.node
+                self.node = node.head
+                self.size -= 1
+                print(self.size)
+                return node
+            else:
+                self.node = None
+                return None
+
+
+a, b, c, d = Node('a'), Node('b'), Node('c'), Node('d')
+stack = Stack()
+stack.add(a)
+stack.add(b)
+stack.add(c)
+stack.add(d)
